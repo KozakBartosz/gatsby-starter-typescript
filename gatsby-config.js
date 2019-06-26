c09275df7d1dc85d94f58bc03dd7d2ef5194ab71
Bartosz Kozak <kozakbartosz94@gmail.com>
2019-06-26 11:35:20 +0200

    start

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Typescript Starter`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    // Add typescript stack into webpack
-    `gatsby-plugin-typescript`
-  ],
-}
+	siteMetadata: {
+		title: `KozakBartosz.pl`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-typescript`,
+	],
+};
