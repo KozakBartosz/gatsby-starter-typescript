f62cfff8acacddce8c2bdf93691e639047bb6c7e
Bartosz Kozak <kozakbartosz94@gmail.com>
2019-07-02 10:16:56 +0200

    fix query

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,35 +1,29 @@
 import React from 'react';
-import {graphql} from 'gatsby'
+import { graphql } from 'gatsby';
 
-
-
-
-export default (data) => {
-    console.log("data",data);
-    const node = data.data.allDatoCmsAbout.edges[0].node
-// export default (data) => {
+export default (data: { data: { allDatoCmsAbout: { nodes: any[] } } }) => {
+	console.log('data', data);
+	const node = data.data.allDatoCmsAbout.nodes[0];
+	// export default (data) => {
 	return (
 		<>
 			<h1>{node.title}</h1>
-			<div>
-                {node.content}
-			</div>
+			<div>{node.content}</div>
 		</>
 	);
 };
 
-
 export const query = graphql`
-query MyQuery {
-    allDatoCmsAbout {
-      edges {
-        node {
-          content
-          title
-        }
-      }
-    }
-}`;
+	query MyQuery {
+		allDatoCmsAbout {
+			nodes {
+				id
+				content
+				title
+			}
+		}
+	}
+`;
 
 // export const pageQuery = graphql`
 // query IndexQuery {
