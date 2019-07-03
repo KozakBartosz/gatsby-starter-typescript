ee762324eaa45d90b195e67e12dd354f85ae8bca
Bartosz Kozak <kozakbartosz94@gmail.com>
2019-07-03 12:13:46 +0200

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,23 @@ module.exports = {
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: `images`,
-				path: path.join(__dirname, `src`, `images`),
+				path: `${__dirname}/src/images/`,
+			},
+		},
+
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `pages`,
+				path: `${__dirname}/src/pages/`,
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `data`,
+				path: `${__dirname}/src/data/`,
+				ignore: [`**/\.*`], // ignore files starting with a dot
 			},
 		},
 		`gatsby-plugin-sharp`,
