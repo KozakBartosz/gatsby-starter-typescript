429046f98dda975b028c7cd5b318a4bdb002fce1
Bartosz Kozak <kozakbartosz94@gmail.com>
2019-10-25 08:32:26 +0200

    kontakt form netlify

--- src/pages/kontakt.tsx
+++ src/pages/kontakt.tsx
@@ -74,6 +74,30 @@ const SecondPage = () => (
 			saepe dolore dolorum accusantium voluptate, quisquam, ea nobis illum facere eveniet
 			libero?
 		</p>
+
+		<form name="contact" netlify netlify-honeypot="bot-field" hidden>
+			<input type="text" name="name" />
+			<input type="email" name="email" />
+			<textarea name="message"></textarea>
+		</form>
+
+		
+		<form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+
 	</>
 );
 
