1cf31f667e985424194607e04fa4e4aaed1c54b1
Bartosz Kozak <kozakbartosz94@gmail.com>
2019-10-25 08:36:42 +0200

    kontakt form netlify

--- src/pages/kontakt.tsx
+++ src/pages/kontakt.tsx
@@ -81,8 +81,8 @@ const SecondPage = () => (
 			<textarea name="message"></textarea>
 		</form>
 
-		
-		<form name="contact" method="post">
+
+		<form name="contact" method="post" netlify>
           <input type="hidden" name="form-name" value="contact" />
           <p>
             <label>Your Name: <input type="text" name="name"/></label>
