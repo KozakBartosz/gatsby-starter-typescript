96b33950234f8003603007239f6806eb702fbd6f
Bartosz Kozak <kozakbartosz94@gmail.com>
2019-10-25 10:55:07 +0200

    thx fix

--- src/pages/thanks.tsx
+++ src/pages/thanks.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 const thxPage = () => (<>
     <h1>Kontakt</h1>
     <p>Welcome to page 2</p>
-    <h1>Super! Thanks for message!</h1>
+    <h1>Thanks for message!</h1>
 
 </>)
 
